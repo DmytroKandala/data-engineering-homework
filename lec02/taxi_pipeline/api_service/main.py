d01186db1e12cbe325e6603ed7b72d80b08d157c
Dmytro <kandala1973@gmail.com>
2025-04-15 23:19:08 +0300

    Fix: use dynamic port from Heroku

--- lec02/taxi_pipeline/api_service/main.py
+++ lec02/taxi_pipeline/api_service/main.py
@@ -11,4 +11,6 @@ def get_rides():
     ])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
